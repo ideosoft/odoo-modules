891569abd1a5bc235fc1156e380b1a865bcce92f
JohnBarneta <jgomez@ideosoft.es>
2016-01-09 16:19:57 +0100

    .

--- ideosoft-helloword/__openerp__.py
+++ ideosoft-helloword/__openerp__.py
@@ -1,10 +1,12 @@
 # -*- coding: utf-8 -*-
 {
     'name': "Hello World",
-    'summary': """
-        Module Hello World""",
-
+    'author': "Ideosoft CB",
+    'version': "1.0",
+    'summary': "Module Hello Word",
     'description': """
         Long description of module's purpose
     """,
+    'depends' : [],
+    'installable': True,
 }
