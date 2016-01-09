9628b12186436adbdd1b8b54ed25ba4a7901e0f1
JohnBarneta <jgomez@ideosoft.es>
2016-01-09 16:14:16 +0100

    add base_es

---
+++ base_es/__openerp__.py
@@ -0,0 +1,18 @@
+{
+    'name' : 'Base ES',
+    'author': 'Ideosoft CB',
+    'version': '1.0',
+    'summary': 'Base ES',
+    'description': """
+The kernel of OpenERP, needed for all installation.
+===================================================
+""",
+    'data': [
+        'res/res.country.state.csv',
+    ],
+    'depends' : ["web"],
+    'qweb': [
+    ],
+    'installable': True,
+    'auto_install': False,
+}
